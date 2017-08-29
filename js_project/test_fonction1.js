f880b963abdbedc28faa13cca2e7f0c0fdb36c20
Valéry Bruniaux <valery.bruniaux@gmail.com>
2017-08-29 15:20:05 +0200

    suite

---
+++ js_project/test_fonction1.js
@@ -0,0 +1,20 @@
+﻿﻿var assert = require('assert');
+var mod = require('./variables4.js');
+
+it("isoNum doit retourner 42", function () {
+  try
+  {
+    assert.equal( 42, mod.isoNum() );
+  }
+  catch (error)
+  {
+    printMessage('Conseil 💡', "Revoyez les fonctions 🤔");
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
+
