70624f2bca308127a48f6855e89165d81e5ef5c6
karclop <41797500+karclop@users.noreply.github.com>
2018-07-27 18:16:10 +0200

    Update test_variables1.js

--- js_project/test_variables1.js
+++ js_project/test_variables1.js
@@ -4,18 +4,18 @@ var mod = require('./variables1.js');
 it("Création des variables : num, txt, bin et flt", function () {
   try
   {
-    assert.equal( 255, mod.num );
+    assert.equal( 123, mod.num );
     assert.equal( typeof(mod.num), "number" );
     assert.equal( "255", mod.txt );
     assert.equal( typeof(mod.txt), "string" );
     assert.equal( true, mod.bin );
     assert.equal( typeof(mod.bin), "boolean" );
-    assert.equal( 7.23, mod.flt );
+    assert.equal( 3.14, mod.flt );
     assert.equal( typeof(mod.flt), "number" );
   }
   catch (error)
   {
-    printMessage('Conseil 💡', "Revoyez la déclaration des variables 🤔");
+    printMessage('Conseil 💡', "Revoyez la déclaration des variables.");
     throw error;
   }
 });
